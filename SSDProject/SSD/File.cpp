3d675e2e6727a743ed26740c1c28f30ac210aadd
yeori977 <yeori977@gmail.com>
2024-04-16 13:51:43 +0900

    [feat] add interface for File

---
+++ SSDProject/SSD/File.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+
+using namespace std;
+
+class IFile {
+public:
+	virtual void read(int lba) = 0;
+	virtual void write(int lba, string data) = 0;
+};
+
+class SSDFile : public IFile {
+public:
+	// IFile을(를) 통해 상속됨
+	void read(int lba) override
+	{
+	}
+
+	void write(int lba, string data) override
+	{
+	}
+
+};
