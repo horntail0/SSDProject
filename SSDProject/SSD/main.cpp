b3259edcbbe44797cebc7f879ec34e6ca098cdbe
hyemin <cheeesecream@naver.com>
2024-04-16 14:08:03 +0900

    feat: add tc for ssd

--- SSDProject/SSD/main.cpp
+++ SSDProject/SSD/main.cpp
@@ -1,4 +1,10 @@
-int main()
+int main(int argc, char* argv[])
 {
 
+}
+
+class ISsd {
+public:
+	void read(){}
+	void write(){}
 }
--- SSDProject/SSD_TC/test.cpp
+++ SSDProject/SSD_TC/test.cpp
@@ -1,7 +1,17 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include "../SSD/File.cpp"
 
-TEST(TestCaseName, TestName) {
+using namespace std;
+using namespace testing;
+
+class MockFile : public IFile {
+public:
+	MOCK_METHOD(void, read, (int), (override));
+	MOCK_METHOD(void, write, (int, string), (override));
+};
+
+TEST(SSDTest, WriteTest) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
