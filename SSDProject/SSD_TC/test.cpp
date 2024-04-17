1cd901af453fc123986f4d5d421c62d76597a850
hyemin <cheeesecream@naver.com>
2024-04-17 14:52:07 +0900

    test: add readtest

--- SSDProject/SSD_TC/test.cpp
+++ SSDProject/SSD_TC/test.cpp
@@ -27,11 +27,6 @@ public:
 	{
 		ssd.setFile(&file);
 	}
-	void TeadDown()
-	{
-		string buf = "del " + NAND_FILE;
-		system(buf.c_str());
-	}
 	MockFile file;
 	SSD ssd;
 };
@@ -42,6 +37,11 @@ public:
 	void SetUp()
 	{
 		ssd.setFile(&file);
+	}
+	void TearDown()
+	{
+		system(("del " + NAND_FILE).c_str());
+		system(("del " + RESULT_FILE).c_str());
 	}
 	string getData(string fileName, int targetLine)
 	{
@@ -102,14 +102,6 @@ TEST_F(MockFixture, WriteTestCallOnce)
 	ssd.write(1, NORMAL_DATA);
 }
 
-TEST_F(MockFixture, ReadTestCallOnce)
-{
-	EXPECT_CALL(file, read(1))
-		.Times(1);
-
-	ssd.read(1);
-}
-
 TEST_F(MockFixture, ReadTestInvalidLba)
 {
 	EXPECT_CALL(file, read(-1))
@@ -121,19 +113,19 @@ TEST_F(MockFixture, ReadTestInvalidLba)
 	ssd.read(100);
 }
 
-TEST_F(SSDFixture, DISABLED_writeFileTest)
+TEST_F(MockFixture, ReadTestCallOnce)
 {
-	string data = "0x12345678";
-	//file.writeFile("test.txt", data);
-	//EXPECT_EQ(data, file.getData("test.txt", 0));
+	EXPECT_CALL(file, read(1))
+		.Times(1);
+
+	ssd.read(1);
 }
 
-TEST_F(SSDFixture, WriteTestNormalDataInit)
+TEST_F(SSDFixture, DISABLED_writeFileTest)
 {
-	string buf = "del " + NAND_FILE;
-	system(buf.c_str());
-	ssd.write(0, NORMAL_DATA);
-	EXPECT_EQ(getData(NAND_FILE, 0), NORMAL_DATA);
+	string data = NORMAL_DATA;
+	//file.writeFile("test.txt", data);
+	//EXPECT_EQ(data, file.getData("test.txt", 0));
 }
 
 TEST_F(SSDFixture, WriteTestNormalData)
@@ -154,13 +146,34 @@ TEST_F(SSDFixture, WriteTestInvalidData)
 	EXPECT_NE(getData(NAND_FILE, 3), DECIMAL_DATA);
 }
 
-TEST_F(SSDFixture, ReadTestFileNormal)
+TEST_F(SSDFixture, ReadTestNormalData)
 {
-	string data = "0x12345678";
-	ssd.write(1, data);
+	ssd.write(1, NORMAL_DATA);
 	ssd.read(1);
+	EXPECT_EQ(getData(RESULT_FILE, 0), NORMAL_DATA);
+}
 
-	string read = getData("result.txt", 1);
+TEST_F(SSDFixture, ReadTestInvalidData)
+{
+	ssd.write(0, LONG_DATA);
+	ssd.read(0);
+	EXPECT_NE(getData(RESULT_FILE, 0), LONG_DATA);
 
-	EXPECT_EQ(read, data);
+	ssd.write(1, SHORT_DATA);
+	ssd.read(1);
+	EXPECT_NE(getData(RESULT_FILE, 0), SHORT_DATA);
+
+	ssd.write(2, NOT_HEXA_DATA);
+	ssd.read(2);
+	EXPECT_NE(getData(RESULT_FILE, 0), NOT_HEXA_DATA);
+
+	ssd.write(3, DECIMAL_DATA);
+	ssd.read(3);
+	EXPECT_NE(getData(RESULT_FILE, 0), DECIMAL_DATA);
+}
+
+TEST_F(SSDFixture, ReadTestBeforeWrite)
+{
+	ssd.read(50);
+	EXPECT_EQ(getData(RESULT_FILE, 0), DEFAULT_DATA);
 }
