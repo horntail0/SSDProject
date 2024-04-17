a957d7941f8be288df001dcb3ae117586dac5ea5
yeori977 <yeori977@gmail.com>
2024-04-17 09:50:31 +0900

    [feat] implement SSD argument handling

--- SSDProject/SSD/main.cpp
+++ SSDProject/SSD/main.cpp
@@ -1,3 +1,22 @@
-﻿int main(int argc, char* argv[])
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
 {
+	string cmd = argv[1];
+	string index = argv[2];
+	int lba = stoi(index);
+
+	if (cmd == "R") {
+		// read
+		cout << "read" << endl;
+	}
+	if (cmd == "W") {
+		// write
+		cout << "write" << endl;
+	}
+
+	return 0;
 }
