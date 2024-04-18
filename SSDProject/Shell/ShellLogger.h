51f2fdb4b9a9a02e106fb6ecb9274531d526b07e
박민수 <pms5917@gmail.com>
2024-04-18 16:06:13 +0900

    [Refactor] Shell Logger 함수 추출 및 const 상수로 대체 하는 등의 refactoring수정을 진행했습니다.

--- SSDProject/Shell/ShellLogger.h
+++ SSDProject/Shell/ShellLogger.h
@@ -9,16 +9,22 @@ using namespace std;
 
 class ShellLogger {
 public:
-	bool recordLog(string functionName, string log);
+	void recordLog(string functionName, string log);
 
 private:
 	string getCurrentTimeString(const string& timeFormat);
 	int getSizeOfLatestLog();
 	string getFormattedLog(const string& functionName, const string& log);
 	string getLastLineOfFile(const string& filePath);
+	void compressOldLogs();
+	bool shouldAddNewLog(const string& logToRecord);
+	void changeLatestLogNameToCurrentTime();
+	void updateLogList(const string& newName);
+	void recordLogToLatestLog(const string& logToRecord);
 
 	const string timeFormatForLogging = "[%y.%m.%d %H:%M:%S]";
 	const string timeFormatForFileName = "%y%m%d_%Hh_%M_%Ss";
 	const string nameOfLatestLog = "latest.log";
 	const string nameOfLogList = "logList.txt";
+	const int MAX_LOG_FILE_SIZE = 10 * 1024; /* 10 KB */
 };
